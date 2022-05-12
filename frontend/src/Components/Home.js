837e7bb5d9428f46bf0d89a768ef520fb99e3b0f
Christina Loiacono <loiaconochristina@gmail.com>
2022-05-12 10:17:03 -0400

    made basic grid layout for home page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import "./Home.css";
 
 function Home() {
   useEffect(() => {
@@ -18,8 +19,22 @@ function Home() {
   }, [])
   return(
       <div id="chartContainer">
-          {/* <div className="tradingview-widget-container"> */}
-          {/* </div> */}
+        <header>
+          <h1>Welcome to App-Name.com</h1>
+          <h4>your education portal for crypto</h4>
+          <hr />
+        </header>
+        <div id="chatbot">
+          Chatbot
+        </div>
+        <div id="chart-coins-container">
+          <div id="chart-widget">
+            BTC Chart or Crypto Market widget
+          </div>
+          <div id="top-coins">
+            Top Coins
+          </div>
+        </div>
       </div>
   )  
 }
