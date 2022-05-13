229a5f7fe8f0df98710d67642ea4f819fbb91ff3
Deborah Campos <631care@gmail.com>
2022-05-13 18:40:13 -0400

    resources table inserted foreign key updated queries & controller

---
+++ backend/queries/resources.js
@@ -0,0 +1,13 @@
+const db = require("../db/dbConfig.js");
+
+const getAllResources = async (users_id)=>{}
+const getOneResource = async (id)=>{}
+const updateResource = async ()=>{}
+const deleteResource = async ()=>{}
+
+module.exports={
+    getAllResources,
+    getOneResource,
+    updateResource,
+    deleteResource
+}
