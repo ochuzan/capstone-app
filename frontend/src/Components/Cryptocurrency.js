114275bc5f2f5ec4a87903f671fc99a3e5617686
Oliver Chuzan <ochuzan@gmail.com>
2022-05-13 10:34:56 -0400

    Created CSS files

--- frontend/src/Components/Cryptocurrency.js
+++ frontend/src/Components/Cryptocurrency.js
@@ -1,7 +1,28 @@
+import "./Cryptocurrency.css"
+import { useEffect } from "react";
+
 const Cryptocurrency = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-screener.js'
+    script.async = true;
+    script.innerHTML = JSON.stringify(
+      {
+        "width": "100%",
+        // "height": "100%",
+        "defaultColumn": "overview",
+        "screener_type": "crypto_mkt",
+        "displayCurrency": "USD",
+        "colorTheme": "dark",
+        "locale": "en"
+      }
+    )
+    document.getElementById("chartContainer").appendChild(script);
+  }, []);
     return(
       <div id="cryptocurrency">
         <h1>Cryptocurrency</h1>
+        <div id="chartContainer"></div>
       </div>
     )
 }
