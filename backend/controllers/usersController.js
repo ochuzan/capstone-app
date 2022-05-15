701647a83a988516b1ae9732dd98c2cad286e793
Milton Clark <kingcoder247@gmail.com>
2022-05-15 10:57:34 -0400

    Create createUser for queries and usersController

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const users = express.Router();
 const {
+    createUser,
     getOneUser,
 } = require("../queries/users.js");
+
+users.post("/", async(req, res) => {
+    const { body } = req;
+    try{
+        const createdUser = await createUser(body);
+        if(createdUser.id){
+            res.status(200).json(createdUser);
+        } else {
+            res.status(422).json("Error: User creation error");
+        }
+    } catch(err){
+        console.log(err);
+    }
+});
 
 users.get("/:id", async(req, res) => {
     const { id } = req.params;
@@ -17,3 +32,5 @@ users.get("/:id", async(req, res) => {
         return err;
     }
 });
+
+module.exports = users;
--- backend/queries/users.js
+++ backend/queries/users.js
@@ -1,14 +1,33 @@
 const db = require("../db/dbConfig.js");
 
+const createUser = async(user) => {
+    try{
+        const newUser = await db.one(
+            "INSERT INTO users (username, password, contact_email, active) VALUES($1, $2, $3, $4) RETURNING *",
+            [
+                user.username,
+                user.password,
+                user.contact_email,
+                user.active
+            ]
+        );
+        return newUser;
+    } catch(err){
+        return err;
+    };
+};
+
 const getOneUser = async(id) => {
     try{
         const oneUser = await db.one("SELECT * FROM users WHERE id=$1", id);
         return oneUser;
     } catch(err){
         return err;
-    }
+    };
 };
 
 module.exports = {
     getOneUser,
+    createUser,
+    
 }
