4e8504561614ffe1bb9d9bdbf12eedc427bc4a32
Oliver Chuzan <ochuzan@gmail.com>
2022-05-16 00:00:18 -0400

    Resources queries file completed

--- backend/queries/resources.js
+++ backend/queries/resources.js
@@ -1,9 +1,61 @@
 const db = require("../db/dbConfig.js");
 
-const getAllResources = async (users_id)=>{}
-const getOneResource = async (id)=>{}
-const updateResource = async ()=>{}
-const deleteResource = async ()=>{}
+const getAllResources = async (users_id) => {
+    try {
+        const allResources = await db.any(
+            "SELECT * FROM resources WHERE users_id=$1",
+            users_id
+        );
+        return allResources;
+    } catch (error) {
+        return error;
+    }
+};
+
+const getOneResource = async (id) => {
+    try {
+        const oneResource = await db.one(
+            "SELECT * FROM resources WHERE id=$1",
+            id
+        );
+        return oneResource;
+    } catch (error) {
+        return error;
+    }
+};
+
+const updateResource = async (id, resource) => {
+    try {
+        const updatedResource = await db.one(
+            "UPDATE resources SET subject=$1, description=$2, type=$3, url=$4, articles_id=$5, is_favorite=$6 WHERE id=$7 RETURNING *",
+            [
+                resource.subject,
+                resource.description,
+                resource.type,
+                resource.url,
+                resource.articles_id,
+                resource.is_favorite,
+                id
+            ]
+        )
+        return updatedResource;
+    } catch (error) {
+        return error;
+    }
+};
+
+// Do we need delete resource? Will regular users be able to delete or admin only?
+const deleteResource = async (id) => {
+    try {
+        const deletedResource = await db.one(
+            "DELETE FROM resources WHERE id=$1 RETURNING *",
+            id
+        );
+        return deletedResource;
+    } catch (error) {
+        return error;
+    }
+};
 
 module.exports={
     getAllResources,
