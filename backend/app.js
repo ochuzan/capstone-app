a313885ecd658b98921be2ad766fa80711c11bb3
Deborah Campos <631care@gmail.com>
2022-05-16 15:14:24 -0400

    commit backend changes from dev branch

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const usersController = require("./controllers/usersController.js");
+const resourcesController = require("./controllers/resourcesController");
 
 app.use(cors());
 app.use(express.json());
