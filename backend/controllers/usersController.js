809f349b432ea0d3ed82a8e8ff2d8f9b91f038f9
Christina Loiacono <loiaconochristina@gmail.com>
2022-05-16 10:57:01 -0400

    connecting controllers to app.js

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -7,6 +7,9 @@ const {
     deleteUser,
 } = require("../queries/users.js");
 
+const resourcesController = require("./resourcesController.js");
+users.use("/:usersId/resources", resourcesController);
+
 users.post("/", async(req, res) => {
     const { body } = req;
     try{
