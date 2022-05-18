cb9f5795b59909e840be0213bd8f0d0f95ead262
Deborah Campos <631care@gmail.com>
2022-05-18 11:48:16 -0400

    is_favorite key value pair added to news, vidoes, and articles

--- frontend/src/data/articlesData.js
+++ frontend/src/data/articlesData.js
@@ -1,32 +1,38 @@
 let articlesData = [
     { 
         source: "What is Bitcoin?", 
-        url:"https://www.bitcoin.com/get-started/what-is-bitcoin/"
+        url:"https://www.bitcoin.com/get-started/what-is-bitcoin/",
+        is_favorite: true
     },
 
     {
         source: "How to Buy Bitcoin", 
-        url:"https://www.bitcoin.com/get-started/how-to-buy-bitcoin/"
+        url:"https://www.bitcoin.com/get-started/how-to-buy-bitcoin/",
+        is_favorite: false
     },
 
     {
         source: "How to Sell Bitcoin", 
-        url: "https://www.bitcoin.com/get-started/how-to-sell-bitcoin/"
+        url: "https://www.bitcoin.com/get-started/how-to-sell-bitcoin/",
+        is_favorite: true
     },
 
     {
         source: "Crypto Ban in Aftica", 
-        url:"https://www.reuters.com/world/africa/central-african-bank-regulator-reminds-states-crypto-ban-2022-05-13/"
+        url:"https://www.reuters.com/world/africa/central-african-bank-regulator-reminds-states-crypto-ban-2022-05-13/",
+        is_favorite: false
     },
 
     {
         source: "What is a Bitcoin Wallet?", 
-        url:"https://www.bitcoin.com/get-started/what-is-a-bitcoin-wallet/"
+        url:"https://www.bitcoin.com/get-started/what-is-a-bitcoin-wallet/",
+        is_favorite: true
     },
 
     {
         source: "What is Ethereum Used For?", 
-        url:"https://www.bitcoin.com/get-started/what-is-eth-used-for/"
+        url:"https://www.bitcoin.com/get-started/what-is-eth-used-for/",
+        is_favorite: false
     }
     // ,
 
