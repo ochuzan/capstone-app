f4184bfdfa3d5dc55133e47d8b1748f3315256c4
Christina Loiacono <loiaconochristina@gmail.com>
2022-05-20 11:48:49 -0400

    tested all routes for all 3 tables in postman.

--- backend/controllers/favoritesController.js
+++ backend/controllers/favoritesController.js
@@ -3,8 +3,8 @@ const favorites = express.Router({mergeParams: true});
 const { 
     getAllFavoritesForUser,
     getOneUsersFavoriteByFavoriteId,
-    // createNewFavorite,
-    // deleteFavorite
+    createNewFavorite,
+    deleteFavorite
 } = require("../queries/favorites.js");
 
 // Get all favorites for a user - Example: http://localhost:3333/users/1/favorites 
@@ -39,30 +39,30 @@ favorites.get("/:id", async (req, res)=>{
 
 
 // Creating a favorite - http://localhost:3333/users/1/favorites/
-// favorites.post("/", async(req, res) => {
-//     const { body } = req;
-//     try{
-//         const createdResource = await createNewFavorite(body);
-//         if(createdResource.id){
-//             res.status(200).json(createdResource);
-//         } else {
-//             res.status(422).json({ error: "Favorites creation error" });
-//         }
-//     } catch(err){
-//         console.log(err);
-//     }
-// });
+favorites.post("/", async(req, res) => {
+    const { body } = req;
+    try{
+        const createdResource = await createNewFavorite(body);
+        if(createdResource.id){
+            res.status(200).json(createdResource);
+        } else {
+            res.status(422).json({ error: "Favorites creation error" });
+        }
+    } catch(err){
+        console.log(err);
+    }
+});
 
 // Deleting a favorite by its ID - Example: http://localhost:3333/users/1/favorites/8
-// favorites.delete("/:id", async (req, res) => {
-// const { id } = req.params;
+favorites.delete("/:id", async (req, res) => {
+const { id } = req.params;
 
-// const deletedFavorite = await deleteFavorite(id);
-// if (deletedFavorite.id) {
-//     res.status(200).json(deletedFavorite);
-// } else {
-//     res.status(404).json({ error: "Favorite not found" });
-// }
-// });
+const deletedFavorite = await deleteFavorite(id);
+if (deletedFavorite.id) {
+    res.status(200).json(deletedFavorite);
+} else {
+    res.status(404).json({ error: "Favorite not found" });
+}
+});
 
 module.exports = favorites;
--- backend/queries/favorites.js
+++ backend/queries/favorites.js
@@ -37,41 +37,41 @@ const getOneUsersFavoriteByFavoriteId = async (id) => {
 //     resource_id
 //     user_id
 // }
-// const createNewFavorite = async (favorite) => {
-//     try {
-//         const newFavorite = await db.one(
-//             `INSERT INTO favorites (favorited_date, resources_id, users_id) VALUES($1, $2, $3) RETURNING *`,
-//             [
-//                 favorite.favorited_date,
-//                 favorite.resources_id,
-//                 favorite.users_id,
-//             ]
-//         );
-//         return newFavorite;
-//     } catch (error) {
-//         return error;
-//     }
-// };
+const createNewFavorite = async (favorite) => {
+    try {
+        const newFavorite = await db.one(
+            `INSERT INTO favorites (favorited_date, resources_id, users_id) VALUES($1, $2, $3) RETURNING *`,
+            [
+                favorite.favorited_date,
+                favorite.resources_id,
+                favorite.users_id,
+            ]
+        );
+        return newFavorite;
+    } catch (error) {
+        return error;
+    }
+};
 
 // Deleting a favorite by its ID - Example: http://localhost:3333/users/1/favorites/8
 // delete by user_id and the resources_id - find the favorite and then delete it
-// const deleteFavorite = async (id) => {
-//     try {
-//         const deletedFavorite = await db.one(
-//             `DELETE FROM favorites 
-//             WHERE id=${id} 
-//             RETURNING *`,
-//             id
-//         );
-//         return deletedFavorite;
-//     } catch (error) {
-//         return error;
-//     }
-// };
+const deleteFavorite = async (id) => {
+    try {
+        const deletedFavorite = await db.one(
+            `DELETE FROM favorites 
+            WHERE id=${id} 
+            RETURNING *`,
+            id
+        );
+        return deletedFavorite;
+    } catch (error) {
+        return error;
+    }
+};
 
 module.exports={
     getAllFavoritesForUser,
     getOneUsersFavoriteByFavoriteId,
-    // createNewFavorite,
-    // deleteFavorite
+    createNewFavorite,
+    deleteFavorite
 }
