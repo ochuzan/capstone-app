32bc29e4db0acceea8abee14c945de31e1b1a199
Oliver Chuzan <ochuzan@gmail.com>
2022-05-22 22:30:53 -0400

    Working get userByUsername query,controller, axios get request

--- frontend/src/Components/UserProfile/Profile.js
+++ frontend/src/Components/UserProfile/Profile.js
@@ -1,6 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
 function Profile() {
+    const [ user, setUser] = useState({});
+
+    const API = process.env.REACT_APP_API_URL;
+    // const { id } = useParams();
+    // let navigate = useNavigate();
+    const username = atob(localStorage.getItem('username').replace(/"/g, ""));
+    // const username = "Christina_234"
+    // console.log(username)
+
+    useEffect(() => {
+        axios.get(`${API}/users/${username}`)
+            .then((res) => {
+                setUser(res.data);
+                console.log(res.data)
+            }).catch((error) => {
+                console.log(error);
+            })
+    }, [])
     return (
-        <h1>Profile Page</h1>
+        <div>
+            <h2>Profile Page</h2>
+            <div>{user.firstname}</div>
+            <div>{user.lastname}</div>
+            <div>{user.username}</div>
+            <div>{user.contact_email}</div>
+        </div>
+
     )
 }
 
