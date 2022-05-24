61422a30c1fd4fb02360a795d0ba842034509aca
Deborah Campos <631care@gmail.com>
2022-05-24 10:18:53 -0400

    added command to bot and commented out videos

--- frontend/src/Components/ChatBot/AlanBot.js
+++ frontend/src/Components/ChatBot/AlanBot.js
@@ -2,7 +2,9 @@ import alanBtn from '@alan-ai/alan-sdk-web';
 import { useEffect } from "react";
 const { REACT_APP_API_ARTICLE_KEY } = process.env
 
+
 function AlanBot(){
+  
     useEffect(() => {
         alanBtn({
             key: REACT_APP_API_ARTICLE_KEY,
@@ -10,7 +12,10 @@ function AlanBot(){
               if (commandData.command === 'go:back') {
                 // Call the client code that will react to the received command
               }
-            }
+              if(commandData.command === 'Good Afternoon'){
+                alert('this is CryptoTalk, a voice assistant for learning about cryptocurrencies. Start by saying: What can I do with this app?')
+              }
+            },
         });
       }, []);
 }
