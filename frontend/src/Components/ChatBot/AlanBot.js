2ab5cb2dba703f3c291b3f71d3228b806dae3494
Deborah Campos <631care@gmail.com>
2022-05-24 17:17:39 -0400

    bot navigation added

--- frontend/src/Components/ChatBot/AlanBot.js
+++ frontend/src/Components/ChatBot/AlanBot.js
@@ -1,9 +1,13 @@
 import alanBtn from '@alan-ai/alan-sdk-web';
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
 const { REACT_APP_API_ARTICLE_KEY } = process.env
 
 
+
+
 function AlanBot(){
+  const navigate = useNavigate();
   
     useEffect(() => {
         alanBtn({
@@ -12,8 +16,28 @@ function AlanBot(){
               if (commandData.command === 'go:back') {
                 // Call the client code that will react to the received command
               }
-              if(commandData.command === 'Good Afternoon'){
-                alert('this is CryptoTalk, a voice assistant for learning about cryptocurrencies. Start by saying: What can I do with this app?')
+              if(commandData.command === 'videos'){
+                console.log(" to videos")
+                navigate('/videos/1')
+              }
+              if(commandData.command === 'news'){
+                console.log('to news')
+                navigate('/news/1')
+              }
+              if(commandData.command === 'articles'){
+                navigate('/articles/1')
+              }
+              if(commandData.command === 'home'){
+                navigate('/')
+              }
+              if(commandData.command === 'create-profile'){
+                navigate('/create-profile')
+              }
+              if(commandData.command === 'profile-page'){
+                navigate('/edit-profile')
+              }
+              if(commandData.command === 'view-profile'){
+                navigate('/profile')
               }
             },
         });
