b6621c31678a276fbcaa3c48f34f832fbc2561e4
Christina Loiacono <loiaconochristina@gmail.com>
2022-05-28 14:15:12 -0400

    NewsPage can display any article from NewsArticle, based on the user/id/news

--- frontend/src/Components/NewsArticle.js
+++ frontend/src/Components/NewsArticle.js
@@ -1,31 +1,23 @@
 import { Link } from "react-router-dom";
 
-
-function NewsArticle({ resource, id }) {
-
+function NewsArticle({ resource }) {
   return (
     <article>
-         <header>
-          <Link to={`/users/${resource.users_id}/news/${resource.id}`}><h3 className="hvr-grow">{resource.name}</h3></Link>
-        </header>
-        <section className="article-preview"> 
-            <div>
-                {/* Date: {resource.date} - Views: {resource.views} */}
-
-            </div>
-            {/* <div>
-                <img className="news-image" src={resource.photo} alt="coins for crypto" />
-            </div> */}
-            <br />
-            <iframe id="inlineFrameExample"
-              title="Inline Frame Example"
-              width="450"
-              height="250"
-              src={resource.url}>
-            </iframe>
-            <br />
-            <h3>Crypto News Favorite?{resource.is_favorite ? "💰 YES " : "📈 NO"}</h3>
-        </section>
+      <header>
+        <Link to={`/users/${resource.users_id}/news/${resource.id}`}>
+          <h3 className="hvr-grow">{resource.name}</h3>
+        </Link>
+      </header>
+      <section className="article-preview">
+        <br />
+        <iframe
+          id="inlineFrameExample"
+          title="Inline Frame Example"
+          width="450"
+          height="250"
+          src={resource.url}
+        ></iframe>
+      </section>
     </article>
   );
 }
--- frontend/src/Components/NewsPage.js
+++ frontend/src/Components/NewsPage.js
@@ -1,43 +1,44 @@
-import { useParams, Link } from "react-router-dom"
-import newsData from "../data/newsData";
-import { useState } from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
+const API = process.env.REACT_APP_API_URL;
 
-function NewsPage(){
-    const [is_favorite, setFavorite] = useState(false)
-    const { id } = useParams();
-    let links = newsData.map((article)=>{
-        let index = newsData.indexOf(article);
-        return(
-            <li><Link to={`/news/${index}`}><h3 className="hvr-grow">{article.name}</h3></Link></li>
-        )
-    })
+function NewsPage() {
+  const [resources, setResources] = useState([]);
+  const { id } = useParams();
 
-    const handleCheckboxChange = () => {
-        setFavorite(!is_favorite);
-    }
-    return(
-        <div>
-            <h1>{newsData[id].name}</h1>
-            <article id="article">
-                <ul className="vid-container">{links}</ul>
-                <div className="vid-container">
-                    <iframe id="inlineFrame"
-                        title={newsData[id].name}
-                        width="750"
-                        height="750"
-                        src={`${newsData[id].url}`} >
-                    </iframe>
-                </div>
-                <div className="vid-container">
-                <form>
-                <h3>FAVORITE</h3> {is_favorite ? "💰 YES" : "📈 NO"}
-                <span><input type="checkbox" onChange={handleCheckboxChange} checked={newsData[id].is_favorite} /></span>
-                </form>
-                </div>
-            </article>
+  useEffect(() => {
+    axios
+      .get(`${API}/users/${id}/resources/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setResources(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{resources.name}</h1>
+      <article id="article">
+        <ul className="vid-container">
+            List of Articles
+        </ul>
+        <div className="vid-container">
+          <iframe
+            id="inlineFrame"
+            title={resources.name}
+            width="750"
+            height="750"
+            src={`${resources.url}`}
+          ></iframe>
         </div>
-    )
+      </article>
+    </div>
+  );
 }
 
 export default NewsPage;
