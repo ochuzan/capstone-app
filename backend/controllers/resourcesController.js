538de761078c9f54f5eb360eb21d9b74d182d22e
Oliver Chuzan <ochuzan@gmail.com>
2022-06-03 12:25:01 -0400

    Integrated test backend code and current code

--- backend/controllers/resourcesController.js
+++ backend/controllers/resourcesController.js
@@ -1,31 +1,21 @@
 const express = require("express");
-const resources = express.Router({mergeParams: true});
-const { getAllResources, 
-    getOneResource, 
-    createResource, 
-    updateResource, 
-    deleteResource 
-} = require("../queries/resources.js");
+const resources = express.Router({mergeParams: true})
 
-// Method GET      `users/{id}/resources/`   Returns the details of all resources with an id of {id}.
-// Example: http://localhost:3333/users/2/resources
+const { getAllResources, getOneResource } = require("../queries/resources.js");
+
 resources.get('/', async (req, res)=>{
-    const { usersId } = req.params;
     try {
-        const allResources = await getAllResources(usersId);
+        const allResources = await getAllResources();
         if (allResources[0]){
             res.status(200).json(allResources);
         } else {
-            res.status(500).json({ error: "no resources found." });
+            res.status(500).json({ error: "No Resources Found." });
         }
     } catch (error){
         console.log(error);
     }
 });
 
-
-// Method GET      `/users/{id}/resources/{id}`  Return details of one `resource` associated with the `users` with an id of {id}
-// Example: http://localhost:3333/users/1/resources/3
 resources.get("/:id", async (req, res)=>{
     const { id } = req.params;
     try {
@@ -33,56 +23,86 @@ resources.get("/:id", async (req, res)=>{
         if (getResource.id){
             res.status(200).json(getResource);
         } else {
-            res.status(500).json({ error: "resource not found!" });
+            res.status(500).json({ error: "Resource Not Found!" });
         }
     } catch (error){
         console.log(error);
-    }
-})
-
-// This might be a feature later on - for a user to post a resource
-// http://localhost:3333/users/2/resources/
-resources.post("/", async(req, res) => {
-    const { body } = req;
-    try{
-        const createdResource = await createResource(body);
-        if(createdResource.id){
-            res.status(200).json(createdResource);
-        } else {
-            res.status(422).json("Error: Resource creation error");
-        }
-    } catch(err){
-        console.log(err);
     }
 });
 
-// Update resource
-// http://localhost:3333/users/2/resources/3
-resources.put("/:id", async (req, res)=>{
-    const { id } = req.params;
-    const { body } = req;
-    try {
-        const updateOneResource = await updateResource(id, body);
-        if (updateOneResource.id){
-            res.status(200).json(updateOneResource);
-        } else {
-            res.status(404).json({ error: "resource not found" });
-        }
-    } catch (error){
-        console.log(error);
-    }
-})
+// // Method GET      `users/{id}/resources/`   Returns the details of all resources with an id of {id}.
+// // Example: http://localhost:3333/users/2/resources
+// resources.get('/', async (req, res)=>{
+//     const { usersId } = req.params;
+//     try {
+//         const allResources = await getAllResources(usersId);
+//         if (allResources[0]){
+//             res.status(200).json(allResources);
+//         } else {
+//             res.status(500).json({ error: "no resources found." });
+//         }
+//     } catch (error){
+//         console.log(error);
+//     }
+// });
 
-// Delete resource
-// http://localhost:3333/users/2/resources/3
-resources.delete("/:id", async (req, res)=>{
-    const { id } = req.params;
-    const deletedResource = await deleteResource(id);
-    if (deletedResource.id){
-        res.status(200).json(deletedResource);
-    } else {
-        res.status(404).json({ error: "resource to delete not found" }); 
-    }
-})
+// // Method GET      `/users/{id}/resources/{id}`  Return details of one `resource` associated with the `users` with an id of {id}
+// // Example: http://localhost:3333/users/1/resources/3
+// resources.get("/:id", async (req, res)=>{
+//     const { id } = req.params;
+//     try {
+//         const getResource = await getOneResource(id);
+//         if (getResource.id){
+//             res.status(200).json(getResource);
+//         } else {
+//             res.status(500).json({ error: "resource not found!" });
+//         }
+//     } catch (error){
+//         console.log(error);
+//     }
+// })
+// // This might be a feature later on - for a user to post a resource
+// // http://localhost:3333/users/2/resources/
+// resources.post("/", async(req, res) => {
+//     const { body } = req;
+//     try{
+//         const createdResource = await createResource(body);
+//         if(createdResource.id){
+//             res.status(200).json(createdResource);
+//         } else {
+//             res.status(422).json("Error: Resource creation error");
+//         }
+//     } catch(err){
+//         console.log(err);
+//     }
+// });
 
+// // Update resource
+// // http://localhost:3333/users/2/resources/3
+// resources.put("/:id", async (req, res)=>{
+//     const { id } = req.params;
+//     const { body } = req;
+//     try {
+//         const updateOneResource = await updateResource(id, body);
+//         if (updateOneResource.id){
+//             res.status(200).json(updateOneResource);
+//         } else {
+//             res.status(404).json({ error: "resource not found" });
+//         }
+//     } catch (error){
+//         console.log(error);
+//     }
+// })
+
+// // Delete resource
+// // http://localhost:3333/users/2/resources/3
+// resources.delete("/:id", async (req, res)=>{
+//     const { id } = req.params;
+//     const deletedResource = await deleteResource(id);
+//     if (deletedResource.id){
+//         res.status(200).json(deletedResource);
+//     } else {
+//         res.status(404).json({ error: "resource to delete not found" }); 
+//     }
+// })
 module.exports = resources;
--- backend/queries/resources.js
+++ backend/queries/resources.js
@@ -1,21 +1,15 @@
 const db = require("../db/dbConfig.js");
 
-// Method GET      `users/{id}/resources/`   Returns the details of all resources with an id of {id}.
-// Example: http://localhost:3333/users/2/resources
-const getAllResources = async(users_id) => {
+const getAllResources = async() => {
     try {
         const allResources = await db.any(
-            "SELECT * FROM resources WHERE users_id=$1",
-            users_id
+            "SELECT * FROM resources"
         )
         return allResources;
     } catch (error) {
         return error;
     }
-}
-
-// Method GET      `/users/{id}/resources/{id}`  Return details of one `resource` associated with the `users` with an id of {id}
-// Example: http://localhost:3333/users/1/resources/3
+};
 const getOneResource = async (id) => {
     try {
         const oneResource = await db.one(
@@ -28,67 +22,99 @@ const getOneResource = async (id) => {
     }
 };
 
-// This might be a feature later on - for a user to create a resource
-// http://localhost:3333/users/2/resources/
-const createResource = async(resource) => {
-    try{
-        const newResource = await db.one(
-            "INSERT INTO resources (name, type, category, url, is_favorite, users_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
-            [
-                resource.name,
-                resource.type,
-                resource.category,
-                resource.url,
-                resource.is_favorite,
-                resource.users_id,
-            ]
-        );
-        return newResource;
-    } catch(err){
-        return err;
-    };
-};
-
-// Should this only update the is_favorite field since they shouldn't update any articles
-// http://localhost:3333/users/2/resources/3
-const updateResource = async (id, resource) => {
-    try {
-        const updatedResource = await db.one(
-            "UPDATE resources SET name=$1, type=$2, category=$3, url=$4, is_favorite=$5, users_id=$6 WHERE id=$7 RETURNING *",
-            [
-                resource.name,
-                resource.type,
-                resource.category,
-                resource.url,
-                resource.is_favorite,
-                resource.users_id,
-                id
-            ]
-        )
-        return updatedResource;
-    } catch (error) {
-        return error;
-    }
-};
-
-// Do we need delete resource? Will regular users be able to delete or admin only?
-// // http://localhost:3333/users/2/resources/3
-const deleteResource = async (id) => {
-    try {
-        const deletedResource = await db.one(
-            "DELETE FROM resources WHERE id=$1 RETURNING *",
-            id
-        );
-        return deletedResource;
-    } catch (error) {
-        return error;
-    }
-};
-
-module.exports={
+module.exports = {
     getAllResources,
-    getOneResource,
-    createResource,
-    updateResource,
-    deleteResource
+    getOneResource
 }
+// Method GET      `users/{id}/resources/`   Returns the details of all resources with an id of {id}.
+// Example: http://localhost:3333/users/2/resources
+// const getAllResources = async(users_id) => {
+//     try {
+//         const allResources = await db.any(
+//             "SELECT * FROM resources WHERE users_id=$1",
+//             users_id
+//         )
+//         return allResources;
+//     } catch (error) {
+//         return error;
+//     }
+// }
+
+// // Method GET      `/users/{id}/resources/{id}`  Return details of one `resource` associated with the `users` with an id of {id}
+// // Example: http://localhost:3333/users/1/resources/3
+// const getOneResource = async (id) => {
+//     try {
+//         const oneResource = await db.one(
+//             "SELECT * FROM resources WHERE id=$1",
+//             id
+//         );
+//         return oneResource;
+//     } catch (error) {
+//         return error;
+//     }
+// };
+
+// // This might be a feature later on - for a user to create a resource
+// // http://localhost:3333/users/2/resources/
+// const createResource = async(resource) => {
+//     try{
+//         const newResource = await db.one(
+//             "INSERT INTO resources (name, type, category, url, is_favorite, users_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+//             [
+//                 resource.name,
+//                 resource.type,
+//                 resource.category,
+//                 resource.url,
+//                 resource.is_favorite,
+//                 resource.users_id,
+//             ]
+//         );
+//         return newResource;
+//     } catch(err){
+//         return err;
+//     };
+// };
+
+// // Should this only update the is_favorite field since they shouldn't update any articles
+// // http://localhost:3333/users/2/resources/3
+// const updateResource = async (id, resource) => {
+//     try {
+//         const updatedResource = await db.one(
+//             "UPDATE resources SET name=$1, type=$2, category=$3, url=$4, is_favorite=$5, users_id=$6 WHERE id=$7 RETURNING *",
+//             [
+//                 resource.name,
+//                 resource.type,
+//                 resource.category,
+//                 resource.url,
+//                 resource.is_favorite,
+//                 resource.users_id,
+//                 id
+//             ]
+//         )
+//         return updatedResource;
+//     } catch (error) {
+//         return error;
+//     }
+// };
+
+// // Do we need delete resource? Will regular users be able to delete or admin only?
+// // // http://localhost:3333/users/2/resources/3
+// const deleteResource = async (id) => {
+//     try {
+//         const deletedResource = await db.one(
+//             "DELETE FROM resources WHERE id=$1 RETURNING *",
+//             id
+//         );
+//         return deletedResource;
+//     } catch (error) {
+//         return error;
+//     }
+// };
+
+// module.exports={
+//     getAllResources,
+//     getOneResource,
+//     createResource,
+//     updateResource,
+//     deleteResource
+// }
