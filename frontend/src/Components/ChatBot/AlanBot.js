df0e96a8e7ab094fa38eee5e2450fa116cac3108
Milton Clark <kingcoder247@gmail.com>
2022-06-03 09:57:01 -0400

    Created news commands for each individual news article

--- frontend/src/Components/ChatBot/AlanBot.js
+++ frontend/src/Components/ChatBot/AlanBot.js
@@ -73,6 +73,33 @@ function AlanBot(){
               if(commandData.command === 'news'){
                 navigate('/news')
               }
+              if(commandData.command === 'news/0'){
+                navigate('/news/0')
+              }
+              if(commandData.command === 'news/1'){
+                navigate('/news/1')
+              }
+              if(commandData.command === 'news/2'){
+                navigate('/news/2')
+              }
+              if(commandData.command === 'news/3'){
+                navigate('/news/3')
+              }
+              if(commandData.command === 'news/4'){
+                navigate('/news/4')
+              }
+              if(commandData.command === 'news/5'){
+                navigate('/news/5')
+              }
+              if(commandData.command === 'news/6'){
+                navigate('/news/6')
+              }
+              if(commandData.command === 'news/7'){
+                navigate('/news/7')
+              }
+              if(commandData.command === 'news/8'){
+                navigate('/news/8')
+              }
             },
         });
       }, []);
